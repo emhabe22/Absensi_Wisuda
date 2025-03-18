05f7d12ee11d2705c4e1a07824fb80bd24c34a4b
emhabe22 <hafillfill87@gmail.com>
2025-03-18 15:43:45 +0700

    Add Fillable

--- app/Http/Controllers/RektoratController.php
+++ app/Http/Controllers/RektoratController.php
@@ -10,45 +10,7 @@ use Illuminate\Http\Request;
 class RektoratController extends Controller
 {
     public function absent($uuid){
-        $data = Rektorat::where('uuid', $uuid)->first();
-        $templatePath = public_path('template/template.png'); 
-    
-        // Path foto mahasiswa
-        $fotoPath = public_path('storage/foto/' . $uuid . '.jpg'); 
-    
-        // Buat gambar dari template
-        $manager = new ImageManager(new Driver());
-        
-        $img = $manager->read($templatePath);
-    
-    
-        // Tambahkan foto mahasiswa (resize dulu)
-        if (file_exists($fotoPath)) {
-            $foto = $manager->read($fotoPath)->resize(150, 150);
-            $img->place($foto, 'top-left', 100, 100); // Sesuaikan posisi
-        }
-    
-        // Tambahkan Nama
-        $img->text($data->nama, 300, 200, function ($font) {
-            $font->file(public_path('fonts/arial.ttf'));
-            $font->size(50);
-            $font->color('#000');
-            $font->align('left');
-        });
-    
-        // Tambahkan NIM
-        $img->text($data->nim, 300, 270, function ($font) {
-            $font->file(public_path('fonts/arial.ttf'));
-            $font->size(40);
-            $font->color('#000');
-            $font->align('left');
-        });
-        // Path penyimpanan hasil gambar
-        $gambarHasil = 'storage/gambar/' . $uuid . '.png';
-        $img->save(public_path($gambarHasil));
-    
-        Session::flash('gambar', asset($gambarHasil));
-    
+        $data = Rektorat::where('uuid', $uuid)->first();    
         //Mahasiswa
         $data->refresh(); // Ambil ulang data dari database sebelum mengubah status
     
@@ -59,7 +21,7 @@ class RektoratController extends Controller
         } else {
             $data->update(['status' => 1]);
             session()->flash('success', 'Absen berhasil!');
-            return redirect('/rektorat')->with('gambar', asset($gambarHasil));
+            return redirect('/rektorat');
         }
     }
 }
